c4e772cfacdd0c3a5cae5a195d08ca3653e4f2ea
Ivan Vasquez <ivan.vasquez@niceincontact.com>
2020-03-25 19:05:33 -0400

    UPDATED

---
+++ ApiFinalExam/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiFinalExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.GetStudents();
+        }
+
+        public void GetStudents()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:8080/student/list");
+
+                var responseTask = client.GetAsync("student");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<Student[]>();
+                    readTask.Wait();
+
+                    var students = readTask.Result;
+
+                    foreach (var student in students)
+                    {
+                        Console.WriteLine($"Student Name: {student.firstName}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ApiFinalExam/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiFinalExam
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string programme { get; set; }
+        public string[] courses { get; set; }
+
+    }
+}
